0292a7f27ed0bb123751bfb8fa5de4d72ad9dfc1
Daniel Togni <dtogni@trialpay.com>
2014-06-05 20:37:17 -0400

    Fixed bulk edit clearing custom fields issue

---
+++ update_repos.sh
@@ -0,0 +1,6 @@
+cd /mnt/redmine/redmine-dt/repos/mobile.git
+git fetch --all
+cd ../combus.git
+git fetch --all
+curl -k "http://ace.trialpay.com:3000/sys/fetch_changesets?key=nCPiQxPypDcGb2wTn9nZ" 
+
