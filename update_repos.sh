5a0020bb300a4870fbb6e7bf65e12084bad5985a
Daniel Togni <dtogni@trialpay.com>
2014-12-10 15:03:28 -0500

    Trialpay specific changes

--- update_repos.sh
+++ update_repos.sh
@@ -1,6 +1,10 @@
-cd /mnt/redmine/redmine-dt/repos/mobile.git
-git fetch --all
-cd ../combus.git
+#!/bin/sh
+PATH=$PATH:/service/app/git/bin
+echo "Updating git repos and fetching changesets into redmine" 
+cd /mnt/redmine/redmine-dt/repos/mobile.git 
+git fetch --all 
+cd /mnt/redmine/redmine-dt/repos/combus.git 
 git fetch --all
 curl -k "http://ace.trialpay.com:3000/sys/fetch_changesets?key=nCPiQxPypDcGb2wTn9nZ" 
+
 
