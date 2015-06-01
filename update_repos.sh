d0b81cd7d7fef5773289d5491a643f84fe8236e5
Daniel Togni <dtogni@trialpay.com>
2015-06-01 14:49:43 -0400

    Updating update script.

--- update_repos.sh
+++ update_repos.sh
@@ -2,11 +2,11 @@
 PATH=$PATH:/service/app/git/bin
 echo "Updating git repos and fetching changesets into redmine" 
 cd /mnt/redmine/redmine-dt/repos/mobile.git 
-git fetch --all 
+git fetch --all --prune
 cd /mnt/redmine/redmine-dt/repos/combus.git 
-git fetch --all
+git fetch --all --prune
 cd /mnt/redmine/redmine-dt/repos/managiant.git 
-git fetch --all
+git fetch --all --prune
 curl -k "https://redmine.trialpay.com/sys/fetch_changesets?key=nCPiQxPypDcGb2wTn9nZ" 
 
 
