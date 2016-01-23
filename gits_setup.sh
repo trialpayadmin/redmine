3f99f1d3c078622e1bf02c0d540b82110c17634e
dtogni <dtogni@trialpay.com>
2016-01-22 16:03:59 -0800

    dev setup script

---
+++ gits_setup.sh
@@ -0,0 +1,20 @@
+cd plugins
+echo Checkout all plugins
+echo Checkout plugin a_common_libs; git clone https://bitbucket.org/dkuk/a_common_libs;
+echo Checkout plugin clipboard_image_paste; git clone https://github.com/peclik/clipboard_image_paste.git;
+echo Checkout plugin redmine-parent-issue-filter; git clone https://github.com/onozaty/redmine-parent-issue-filter.git;
+echo Checkout plugin redmine_better_gantt_chart; git clone https://github.com/kulesa/redmine_better_gantt_chart.git;
+echo Checkout plugin redmine_ckeditor; git clone https://github.com/a-ono/redmine_ckeditor;
+echo Checkout plugin redmine_codebutton; git clone https://github.com/mediatainment/redmine_codebutton.git;
+echo Checkout plugin redmine_cors; git clone git://github.com/mavimo/redmine_cors.git;
+echo Checkout plugin redmine_custom_css; git clone https://github.com/martin-denizet/redmine_custom_css.git;
+echo Checkout plugin redmine_keyboard_shortcuts; git clone https://github.com/netaustin/redmine_keyboard_shortcuts.git;
+echo Checkout plugin redmine_landing_page; git clone https://github.com/biow0lf/redmine_landing_page;
+echo Checkout plugin redmine_local_avatars; git clone https://github.com/ncoders/redmine_local_avatars.git;
+echo Checkout plugin redmine_my_page_queries; git clone git://github.com/Undev/redmine_my_page_queries.git;
+echo Checkout plugin redmine_once_assigned_ever_watcher; git clone https://github.com/trialpayadmin/redmine_once_assigned_ever_watcher;
+echo Checkout plugin redmine_revision_diff; git clone https://github.com/mateusmedeiros/redmine_revision_diff.git;
+echo Checkout plugin redmine_rouge; git clone https://github.com/trialpayadmin/redmine_rouge.git;
+echo Checkout plugin redmine-slack; git clone https://github.com/sciyoshi/redmine-slack.git;
+echo Checkout plugin sidebar_hide; git clone https://github.com/bdemirkir/sidebar_hide.git;
+echo done.
