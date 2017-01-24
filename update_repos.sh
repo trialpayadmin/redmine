03d6ec87c64e8ea3b28d5ac72defacba9ae8dd6e
Daniel Togni <dtogni@trialpay.com>
2017-01-24 17:41:50 -0500

    Adding ghost

--- update_repos.sh
+++ update_repos.sh
@@ -1,20 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 PATH=$PATH:/service/app/git/bin
 echo "Updating git repos and fetching changesets into redmine" 
-cd /mnt/redmine/redmine-dt/repos/mobile.git 
-git fetch --all --prune
-cd /mnt/redmine/redmine-dt/repos/combus.git 
-git fetch --all --prune
-cd /mnt/redmine/redmine-dt/repos/managiant.git 
-git fetch --all --prune
-cd /mnt/redmine/redmine-dt/repos/projects.git 
-git fetch --all --prune
-cd /mnt/redmine/redmine-dt/repos/vcrunch.git 
-git fetch --all --prune
-cd /mnt/redmine/redmine-dt/repos/wallet_msg.git 
-git fetch --all --prune
-cd /mnt/redmine/redmine-dt/repos/datascience.git 
-git fetch --all --prune
+base=/mnt/redmine/redmine-dt/repos
+for repo in $(ls -d $base/*.git) 
+do
+  echo Updating $repo
+  cd $repo && pwd && git fetch --all --prune
+done
+cd $base
 curl -k "https://redmine.trialpay.com/sys/fetch_changesets?key=nCPiQxPypDcGb2wTn9nZ" 
 
 
